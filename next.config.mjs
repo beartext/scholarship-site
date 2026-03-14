32830440f220799388bc452ccdce93336f214ad3
John Rebein <johnprebein@gmail.com>
2026-03-14 17:46:24 +0100

    Disable Turbopack to fix Tailwind build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: false, // disable Turbopack
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
